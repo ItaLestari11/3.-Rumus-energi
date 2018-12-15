4d61b7e476471d0aa7790dc2c48d769a737e7c28
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:23:21 +0700

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int m,c;
+float E=m*c*c;
+
+cout<<"masukan nilai m :";
+cin>>m;
+cout<<"masukan nilai c :";
+cin>>c;
+E=m*c*c;
+cout<<"Rumus energi adalah"<<E;
+return 0;
+}
